ab41b8cc7a44dbebfc617bc12dcd5c43ce257f08
Damian Rieumont <damianaramis00@gmail.com>
2021-09-05 21:23:46 -0400

    redesigned the system of inputs by buttons in a big if else

---
+++ add.js
@@ -0,0 +1,49 @@
+const input = document.querySelector('#text-area');
+let isDecimal = false;
+const operatorsList = {
+    '+': true,
+    '-': true,
+    '*': true,
+    '/': true
+}
+
+export function number(number) {
+    input.value = input.value.concat(number);
+}
+
+export function operator(operator) {
+    if (input.value == '') { //if the input is empty
+        input.value = '0' + operator;
+    } else {
+        if (operatorsList[input.value[input.value.length - 1]] || input.value[input.value.length - 1] == '.') //if the last elemennt in the input is an operator or a dot, its is replced
+            input.value = input.value.slice(0, input.value.length - 1) + operator;
+        else //else is added to the input
+            input.value = input.value + operator;
+    }
+    isDecimal = false;
+}
+
+export function deleteLast() {
+    if (input.value[input.value.length - 1] === '.') {
+        isDecimal = false
+    }
+    input.value = input.value.slice(0, input.value.length - 1);
+}
+
+export function deleteAll() {
+    isDecimal = false;
+    input.value = '';
+}
+
+export function dot() {
+    if (input.value == '') {
+        input.value = '0';
+    } else if (operatorsList[input.value[input.value.length - 1]]) { //if the las input element is an operator add a zero
+        input.value = input.value + '0';
+    }
+
+    if (!isDecimal) {
+        input.value = input.value + '.';
+        isDecimal = true;
+    }
+}
--- main.js
+++ main.js
@@ -1,78 +1,22 @@
-const input = document.querySelector('#text-area');
-let isDecimal = false;
+import * as add from './add.js';
 
-//add a number to the input
-const numbers = document.querySelectorAll('.number');
+document.addEventListener('click', (e) => {
 
-numbers.forEach(number => {
-    number.addEventListener('click', (e) => {
-        addNumber(e);
-    });
-});
-
-function addNumber(e) {
-    input.value = input.value.concat(e.target.innerText);
-}
-
-//add an operator to the input
-const operators = document.querySelectorAll('.operator');
-
-const operatorsList = {
-    '+': true,
-    '-': true,
-    '*': true,
-    '/': true
-}
-
-operators.forEach(operator => {
-    operator.addEventListener('click', (e) => {
-
-        if (input.value == '') { //if the input is empty
-            input.value = '0' + e.target.getAttribute('value');
-        } else {
-            if (operatorsList[input.value[input.value.length - 1]] || input.value[input.value.length - 1] == '.') //if the last elemennt in the input is an operator or a dot, its is replced
-                input.value = input.value.slice(0, input.value.length - 1) + e.target.getAttribute('value');
-            else //else is added to the input
-                input.value = input.value + e.target.getAttribute('value');
-        }
-        isDecimal = false;
-    });
-});
-
-//Delete last input
-const deleteLast = document.querySelector('.delete-last');
-
-deleteLast.addEventListener('click', (e) => {
-    if (input.value[input.value.length - 1] === '.') {
-        isDecimal = false
-    }
-    input.value = input.value.slice(0, input.value.length - 1);
-
-});
-
-//Delete all input
-const deleteAll = document.querySelector('.delete-all');
-
-deleteAll.addEventListener('click', (e) => {
-    isDecimal = false;
-    input.value = '';
-});
-
-//Add a dot '.'
-const dot = document.querySelector('.dot');
-
-dot.addEventListener('click', (e) => {
-    if (input.value == '') {
-        input.value = '0';
-    } else if (operatorsList[input.value[input.value.length - 1]]) { //if the las input element is an operator add a zero
-        input.value = input.value + '0';
+    const classList = e.target.classList;
+    if (classList.contains('number')) {
+        add.number(e.target.innerText); //add a number to the input
+    } else if (classList.contains('operator')) {
+        add.operator(e.target.getAttribute('value')); //add an operator to the input
+    } else if (classList.contains('delete-last')) {
+        add.deleteLast(); //Delete last input
+    } else if (classList.contains('delete-all')) {
+        add.deleteAll(); //Delete all input
+    } else if (classList.contains('dot')) {
+        add.dot(); //Add a dot '.'
     }
 
-    if (!isDecimal) {
-        input.value = input.value + '.';
-        isDecimal = true;
-    }
 });
+
 
 //control input by keyboard
 const allowedKeys = {
@@ -96,7 +40,7 @@ const allowedKeys = {
     '=': 'equal'
 }
 input.addEventListener("input", function(e) {
-    console.log(allowedKeys[e.data])
+    console.log(e)
 
     if (allowedKeys[e.data] === 'number') {
 
